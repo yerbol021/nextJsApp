ded01ecc5562b50f1631829558b112155f058cda
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-10-24 11:49:03 -0700

    Implement getStaticPaths

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,13 +1,14 @@
 import Layout from "../../components/layout";
+import { getAllPostIds } from "../../lib/posts";
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
 export default function Post() {
   return <Layout>...</Layout>;
-}
-
-export async function getStaticPaths() {
-  // Return a list of possible value for id
-}
-
-export async function getStaticProps({ params }) {
-  // Fetch necessary data for the blog post using params.id
 }
